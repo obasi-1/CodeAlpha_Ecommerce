c4214d507debc01f95f75b3b8e0d4128da36a927
Obasi-sam Otei <obasonz61@gmail.com>
2026-09-21 07:08:39 +0100

    Update API URL for production

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://127.0.0.1:8000/api/';
+const apiUrl = 'https://codealpha-ecommerce-cb6s.onrender.com/api/';
 
 // Fetch products from Django and render them
 async function loadProducts() {
