6ec08c9bbf3c528c7538ae82558453a573f83026
Obasi-sam Otei <obasonz61@gmail.com>
2026-09-21 06:56:31 +0100

    Add Render deployment files

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u_qdz6)ps@^-&f4q7fg3u6dpxhob!nx4o3!trfg&z0)zb#_sfv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
